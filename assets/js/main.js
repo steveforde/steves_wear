9fb57771dad6c4534a679c49f4235ef23551cb4c
steveforde <sforde08@gmail.com>
2025-02-25 21:07:10 +0000

    updated links and buttons

--- assets/js/main.js
+++ assets/js/main.js
@@ -156,3 +156,25 @@ document.addEventListener('DOMContentLoaded', function() {
   }, { threshold: 0.1 });
   images.forEach(img => observer.observe(img));
 });
+
+//Get the button:
+mybutton = document.getElementById("scrollToTopBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+mybutton.addEventListener("click", topFunction);
